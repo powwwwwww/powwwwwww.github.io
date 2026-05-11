fd48e8b8d9f80df64387f25d859ad664bd4a722a
powwwwwww <hcho2256@naver.com>
2026-05-11 16:53:24 +0000

    Deploying to gh-pages from @ powwwwwww/powwwwwww.github.io@cb2c60e1f453c28407375c678618002d5767d77a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "powwwwwww/powwwwwww.github.com",
+    "data-repo": "powwwwwww/powwwwwww.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
